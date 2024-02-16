4c254cc1071551101df48979a1b6074508eb5c47
TheChaosStar <caron.donovan1@outlook.fr>
2024-02-16 22:26:34 +0100

    Sprite, Tile and Map

--- main.py
+++ main.py
@@ -2,7 +2,96 @@ import pygame, os
 from prettytable import PrettyTable
 
 # ------------------------------/ Config /------------------------------
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1080, 720
+
+# -----------------------------/ Window /-------------------------------
+class Window():
+    def __init__(self, title:str='', size:tuple=(WIDTH, HEIGHT)):
+        pygame.init()
+        pygame.display.set_caption(title)
+        self.surface = pygame.display.set_mode(size)
+        self.clock = pygame.time.Clock()
+
+        self.event = None
+        self.time_delta = 0
+        self.is_running = True
+
+    def update(self):
+        pygame.display.flip()
+
+        self.time_delta = self.clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            self.event = event
+            if event.type == pygame.QUIT:
+                self.is_running = False
+
+    def draw(self, surface:pygame.Surface, pos:tuple=(0, 0)):
+        self.surface.blit(surface, pos)
+
+# ------------------------------/ Tile /--------------------------------
+class Tile():
+    def __init__(self, image, pos):
+        self.surface = image
+        self.position = pos
+
+# -----------------------------/ Sprite /-------------------------------
+class Sprite():
+    def __init__(self):
+        self.sprite_folder = './sprites'
+
+    def load(self, path:str) -> pygame.Surface:
+        if path[0] == '/': path = self.sprite_folder + path
+        elif path[0] != '.': path = self.sprite_folder + '/' + path
+        return pygame.image.load(path)
+    
+    def tolist(self, surface:pygame.Surface, size:tuple) -> list[Tile]:
+        _list = []
+        sprite_width, sprite_height = surface.get_size()
+        
+        for y in range(0, sprite_height, 16):
+            for x in range(0, sprite_width, 16):
+                _list.append(Sprite().toTile(surface, size, (x, y), (0, 0)))
+                                
+        return _list
+
+    def toTile(self, surface:pygame.Surface, size:tuple, pos_image:tuple, pos_screen:tuple) -> Tile:
+        image = pygame.Surface(size)
+        image.blit(surface, (0, 0), (*pos_image, *size))
+        return Tile(image, pos_screen)
+
+# -------------------------------/ Map /--------------------------------
+class Map():
+    def __init__(self, map_paths:list[str], spritesheet_paths:list[str], sprite_sizes:list[tuple], background_paths:list[str]=None) -> None:
+        self.world = World()
+        default_sprite_size = sprite_sizes[0]
+        default_spritesheet_path = spritesheet_paths[0]
+
+        max_length = max([len(map_paths), len(spritesheet_paths), len(sprite_sizes)])
+
+        if len(spritesheet_paths) == 1:
+            spritesheet_paths = [default_spritesheet_path for _ in range(max_length)]
+        if len(sprite_sizes) == 1:
+            sprite_sizes = [default_sprite_size for _ in range(max_length)]
+
+        self.infos = zip(map_paths, spritesheet_paths, sprite_sizes)
+
+        self.surfaces = [Sprite().load(bg) for bg in background_paths] if background_paths else []
+
+    def load(self):
+        sprite = Sprite()
+
+        for info in self.infos:
+            tiles = sprite.tolist(sprite.load(info[1]), info[2])
+            world = self.world.Read(info[0])
+            surface = pygame.Surface((len(world[0]) * info[2][0], len(world) * info[2][1]))
+            surface.set_colorkey((0, 0, 0))
+
+            for y, row in enumerate(world):
+                for x, index in enumerate(row):
+                    if index:
+                        surface.blit(tiles[index].surface, (x*info[2][0], y*info[2][1]))
+
+            self.surfaces.append(surface)
 
 # ------------------------------/ World /-------------------------------
 class World():
@@ -20,7 +109,7 @@ class World():
 
                 for value in line.rstrip().split():
                     if value == '-1': l.append(None)
-                    else: l.append(value)
+                    else: l.append(int(value))
                 
                 tab.append(l)
         
@@ -70,31 +159,6 @@ class World():
 
         f.close()
 
-# -----------------------------/ Window /-------------------------------
-
-class Window():
-    def __init__(self, title:str='', size:tuple=(WIDTH, HEIGHT)):
-        pygame.init()
-        pygame.display.set_caption(title)
-        self.surface = pygame.display.set_mode(size)
-        self.clock = pygame.time.Clock()
-
-        self.event = None
-        self.time_delta = 0
-        self.is_running = True
-
-    def update(self):
-        self.time_delta = self.clock.tick(60) / 1000.0
-        for event in pygame.event.get():
-            self.event = event
-            if event.type == pygame.QUIT:
-                self.is_running = False
-
-        pygame.display.update()
-
-    def draw(self, surface:pygame.Surface, pos:tuple=(0, 0)):
-        self.surface(surface, pos)
-
 
 # ------------------------------/ Main /--------------------------------
 window = Window()
@@ -102,11 +166,21 @@ window = Window()
 background = pygame.Surface((WIDTH, HEIGHT))
 background.fill(pygame.Color("#000000"))
 
-# World
-world = World()
-# world.Create((10, 10))
-world.Write('map-1', (5, 1), None)
+# Map
+map = Map(
+    ['cave1-backward2', 'cave1-backward', 'cave1-platform', 'cave1-forward'], # map files
+    ['/mainlev_build.png'], # spritesheet
+    [(16, 16)], # sprite size
+    ['/background1.png', '/background2.png', '/background3.png', '/background4a.png'] # backrounds
+) 
+map.load()
+
+print(map.surfaces)
 
 while window.is_running:
     window.update()
+
     window.draw(background)
+
+    for surface in map.surfaces:
+        window.draw(surface)
